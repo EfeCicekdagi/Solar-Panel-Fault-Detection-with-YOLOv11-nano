26f8abf2ad694510eb05700c918a90def62f78d6
Efe Çiçekdağı <147732030+EfeCicekdagi@users.noreply.github.com>
2025-09-11 15:27:01 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+def main():
+    model = YOLO("runs/detect/train/weights/best.pt")
+    metrics = model.val(
+        data="C:/Users/hp/Desktop/AI/Solar Panel Project/Task4_EfeÇiçekdağı/Data/data.yaml",
+        split="test",
+        imgsz=640,
+        batch=16,
+        workers=2,   
+        device=0
+    )
+    print(metrics)
+
+if __name__ == "__main__":
+    from multiprocessing import freeze_support
+    freeze_support()
+    main()

68f7a1182b80f73f6210fc88e20d2a4495618e75
Efe Çiçekdağı <147732030+EfeCicekdagi@users.noreply.github.com>
2025-11-11 14:34:26 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 def main():
     model = YOLO("runs/detect/train/weights/best.pt")
     metrics = model.val(
-        data="C:/Users/hp/Desktop/AI/Solar Panel Project/Task4_EfeÇiçekdağı/Data/data.yaml",
+        data="Write the file extension of .yaml",
         split="test",
         imgsz=640,
         batch=16,
@@ -15,4 +15,5 @@ def main():
 if __name__ == "__main__":
     from multiprocessing import freeze_support
     freeze_support()
+
     main()
